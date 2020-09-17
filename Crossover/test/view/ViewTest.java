876fba16959d87cbb126ad3e82cd7e5603fdb504
Benjamin Wagner <wagnerb4@students.uni-marburg.de>
2020-09-17 13:16:22 +0200

    generated test stubs

---
+++ Crossover/test/view/ViewTest.java
@@ -0,0 +1,232 @@
+/**
+ * 
+ */
+package view;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Benjamin Wagner
+ *
+ */
+class ViewTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link view.View#getNode(org.eclipse.emf.ecore.EObject)}.
+	 */
+	@Test
+	final void testGetNode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#getRandomNode()}.
+	 */
+	@Test
+	final void testGetRandomNode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#getObject(org.eclipse.emf.henshin.model.Node)}.
+	 */
+	@Test
+	final void testGetObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#isEmpty()}.
+	 */
+	@Test
+	final void testIsEmpty() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#extend(org.eclipse.emf.ecore.EClass)}.
+	 */
+	@Test
+	final void testExtendEClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#extend(org.eclipse.emf.ecore.EReference)}.
+	 */
+	@Test
+	final void testExtendEReference() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#extend(org.eclipse.emf.ecore.EObject)}.
+	 */
+	@Test
+	final void testExtendEObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#extend(org.eclipse.emf.ecore.EObject, org.eclipse.emf.ecore.EObject, org.eclipse.emf.ecore.EReference)}.
+	 */
+	@Test
+	final void testExtendEObjectEObjectEReference() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#reduce(org.eclipse.emf.ecore.EClass)}.
+	 */
+	@Test
+	final void testReduceEClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#reduce(org.eclipse.emf.ecore.EReference)}.
+	 */
+	@Test
+	final void testReduceEReference() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#reduce(org.eclipse.emf.ecore.EObject)}.
+	 */
+	@Test
+	final void testReduceEObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#reduce(org.eclipse.emf.ecore.EObject, org.eclipse.emf.ecore.EObject, org.eclipse.emf.ecore.EReference)}.
+	 */
+	@Test
+	final void testReduceEObjectEObjectEReference() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#copy()}.
+	 */
+	@Test
+	final void testCopy() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#clear()}.
+	 */
+	@Test
+	final void testClear() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#union(view.View)}.
+	 */
+	@Test
+	final void testUnion() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#intersect(view.View)}.
+	 */
+	@Test
+	final void testIntersect() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#subtract(view.View)}.
+	 */
+	@Test
+	final void testSubtract() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#removeDangling()}.
+	 */
+	@Test
+	final void testRemoveDangling() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#completeDangling()}.
+	 */
+	@Test
+	final void testCompleteDangling() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#extendByMissingEdges()}.
+	 */
+	@Test
+	final void testExtendByMissingEdges() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link view.View#matchViewByMetamodel(org.eclipse.emf.ecore.resource.Resource)}.
+	 */
+	@Test
+	final void testMatchViewByMetamodel() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
+	 */
+	@Test
+	final void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#toString()}.
+	 */
+	@Test
+	final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
