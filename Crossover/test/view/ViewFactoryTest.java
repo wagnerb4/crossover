05725c276a25fecffcad79ec2e746ca7c20a49d7
Benjamin Wagner <wagnerb4@students.uni-marburg.de>
2020-10-12 18:34:41 +0200

    Fixed imports because of the change to Junit 5.

--- Crossover/test/view/ViewFactoryTest.java
+++ Crossover/test/view/ViewFactoryTest.java
@@ -3,10 +3,10 @@
  */
 package view;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.ArrayList;
--- Crossover/test/view/ViewPackageTest.java
+++ Crossover/test/view/ViewPackageTest.java
@@ -1,7 +1,7 @@
 package view;
 
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import java.util.HashMap;
 import java.util.HashSet;
